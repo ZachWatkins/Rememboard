a6931bfe9c384185e786a8712a066c5470685583
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-11-30 16:50:15 -0600

    Generate Event resource code

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,23 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EventStoreRequest;
+use App\Http\Requests\EventUpdateRequest;
 use App\Models\Event;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Http\JsonResponse;
 
 class EventController extends Controller
 {
-    public function countdowns(Request $request): Response
+    public function index(Request $request): View
     {
-        $events = Event::where('show_on_countdown', $show_on_countdown)->orderBy('start_date')->get();
+        $events = Event::all();
 
         return view('event.index', compact('events'));
     }
 
-    public function trips(Request $request): Response
+    public function create(Request $request): View
     {
-        $events = Event::where('is_trip', $is_trip)->orderBy('start_date')->get();
+        return view('event.create');
+    }
 
-        return $trips;
+    public function store(EventStoreRequest $request): RedirectResponse
+    {
+        $event = Event::create($request->validated());
+
+        $request->session()->flash('event.id', $event->id);
+
+        return redirect()->route('events.index');
+    }
+
+    public function show(Request $request, Event $event): View
+    {
+        return view('event.show', compact('event'));
+    }
+
+    public function edit(Request $request, Event $event): View
+    {
+        return view('event.edit', compact('event'));
+    }
+
+    public function update(EventUpdateRequest $request, Event $event): RedirectResponse
+    {
+        $event->update($request->validated());
+
+        $request->session()->flash('event.id', $event->id);
+
+        return redirect()->route('events.index');
+    }
+
+    public function destroy(Request $request, Event $event): RedirectResponse
+    {
+        $event->delete();
+
+        return redirect()->route('events.index');
+    }
+
+    public function countdowns(): View
+    {
+        $events = Event::where('show_on_countdown', true)->orderBy('start_date')->get();
+
+        return view('event.index', compact('events'));
+    }
+
+    public function trips(Request $request): JsonResponse
+    {
+        $trips = Event::where('is_trip', $request->input('is_trip'))->orderBy('start_date')->get();
+
+        return response()->json($trips);
     }
 }
--- tests/Feature/Http/Controllers/EventControllerTest.php
+++ tests/Feature/Http/Controllers/EventControllerTest.php
@@ -3,7 +3,164 @@
 namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Event;
+use Illuminate\Support\Carbon;
+use function Pest\Faker\fake;
+use function Pest\Laravel\assertModelMissing;
+use function Pest\Laravel\delete;
 use function Pest\Laravel\get;
+use function Pest\Laravel\post;
+use function Pest\Laravel\put;
+
+uses(\JMac\Testing\Traits\AdditionalAssertions::class);
+
+test('index displays view', function (): void {
+    $events = Event::factory()->count(3)->create();
+
+    $response = get(route('events.index'));
+
+    $response->assertOk();
+    $response->assertViewIs('event.index');
+    $response->assertViewHas('events');
+});
+
+test('create displays view', function (): void {
+    $response = get(route('events.create'));
+
+    $response->assertOk();
+    $response->assertViewIs('event.create');
+});
+
+test('store uses form request validation')
+    ->assertActionUsesFormRequest(
+        \App\Http\Controllers\EventController::class,
+        'store',
+        \App\Http\Requests\EventStoreRequest::class
+    );
+
+test('store saves and redirects', function (): void {
+    $name = fake()->name();
+    $description = fake()->text();
+    $start_date = Carbon::parse(fake()->dateTime());
+    $latitude = fake()->latitude();
+    $longitude = fake()->longitude();
+    $city = fake()->city();
+    $state = 'Texas';
+    $folder_name = fake()->word();
+    $show_on_countdown = fake()->boolean();
+    $is_trip = fake()->boolean();
+
+    $response = post(route('events.store'), [
+        'name' => $name,
+        'description' => $description,
+        'start_date' => $start_date->toDateTimeString(),
+        'latitude' => $latitude,
+        'longitude' => $longitude,
+        'city' => $city,
+        'state' => $state,
+        'folder_name' => $folder_name,
+        'show_on_countdown' => $show_on_countdown,
+        'is_trip' => $is_trip,
+    ]);
+    $response->assertSessionHasNoErrors();
+
+    $events = Event::query()
+        ->where('name', $name)
+        ->where('description', $description)
+        ->where('start_date', $start_date)
+        ->where('latitude', $latitude)
+        ->where('longitude', $longitude)
+        ->where('city', $city)
+        ->where('state', $state)
+        ->where('folder_name', $folder_name)
+        ->where('show_on_countdown', $show_on_countdown)
+        ->where('is_trip', $is_trip)
+        ->get();
+    expect($events)->toHaveCount(1);
+    $event = $events->first();
+
+    $response->assertRedirect(route('events.index'));
+    $response->assertSessionHas('event.id', $event->id);
+});
+
+test('show displays view', function (): void {
+    $event = Event::factory()->create();
+
+    $response = get(route('events.show', $event));
+
+    $response->assertOk();
+    $response->assertViewIs('event.show');
+    $response->assertViewHas('event');
+});
+
+test('edit displays view', function (): void {
+    $event = Event::factory()->create();
+
+    $response = get(route('events.edit', $event));
+
+    $response->assertOk();
+    $response->assertViewIs('event.edit');
+    $response->assertViewHas('event');
+});
+
+test('update uses form request validation')
+    ->assertActionUsesFormRequest(
+        \App\Http\Controllers\EventController::class,
+        'update',
+        \App\Http\Requests\EventUpdateRequest::class
+    );
+
+test('update redirects', function (): void {
+    $event = Event::factory()->create();
+    $name = fake()->name();
+    $description = fake()->text();
+    $start_date = Carbon::parse(fake()->dateTime());
+    $latitude = fake()->latitude();
+    $longitude = fake()->longitude();
+    $city = fake()->city();
+    $state = 'Texas';
+    $folder_name = fake()->word();
+    $show_on_countdown = fake()->boolean();
+    $is_trip = fake()->boolean();
+
+    $response = put(route('events.update', $event), [
+        'name' => $name,
+        'description' => $description,
+        'start_date' => $start_date->toDateTimeString(),
+        'latitude' => $latitude,
+        'longitude' => $longitude,
+        'city' => $city,
+        'state' => $state,
+        'folder_name' => $folder_name,
+        'show_on_countdown' => $show_on_countdown,
+        'is_trip' => $is_trip,
+    ]);
+
+    $event->refresh();
+
+    $response->assertRedirect(route('events.index'));
+    $response->assertSessionHas('event.id', $event->id);
+
+    expect($name)->toEqual($event->name);
+    expect($description)->toEqual($event->description);
+    expect($start_date)->toEqual($event->start_date);
+    expect($latitude)->toEqual($event->latitude);
+    expect($longitude)->toEqual($event->longitude);
+    expect($city)->toEqual($event->city);
+    expect($state)->toEqual($event->state);
+    expect($folder_name)->toEqual($event->folder_name);
+    expect($show_on_countdown)->toEqual($event->show_on_countdown);
+    expect($is_trip)->toEqual($event->is_trip);
+});
+
+test('destroy deletes and redirects', function (): void {
+    $event = Event::factory()->create();
+
+    $response = delete(route('events.destroy', $event));
+
+    $response->assertRedirect(route('events.index'));
+
+    assertModelMissing($event);
+});
 
 test('countdowns displays view', function (): void {
     $events = Event::factory()->count(3)->create();
@@ -15,9 +172,8 @@ test('countdowns displays view', function (): void {
     $response->assertViewHas('events');
 });
 
-
 test('trips responds with', function (): void {
-    $events = Event::factory()->count(3)->create();
+    $trips = Event::factory()->count(3)->create();
 
     $response = get(route('events.trips'));
 
