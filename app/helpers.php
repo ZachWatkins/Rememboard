5969d187beeb41a4285d50de99221b06b1bd058b
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-09 22:00:48 -0600

    Create helper function for user timezone conversion to UTC

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,18 @@
 /**
  * This file defines global helper functions.
  */
+
+if (!function_exists('convertToUserTimezone')) {
+    /**
+     * Convert a datetime string to the user's timezone.
+     *
+     * @param string $datetime
+     * @return string
+     */
+    function convertToUserTimezone(string $datetime): string
+    {
+        $userTimezone = \Illuminate\Support\Facades\Auth::user()->timezone; // Assuming the user has a 'timezone' attribute
+        $timestamp = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $datetime, $userTimezone);
+        return $timestamp->setTimezone('UTC')->toDateTimeString();
+    }
+}
