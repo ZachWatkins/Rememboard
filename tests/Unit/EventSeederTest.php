6e431044bfcaeb092e6d28eb5f2d8201165b034d
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-09 22:22:59 -0600

    Tidy comments

--- tests/Unit/EventSeederTest.php
+++ tests/Unit/EventSeederTest.php
@@ -4,19 +4,14 @@ use App\Models\Event;
 use Database\Seeders\EventSeeder;
 
 it('imports a JSON file', function () {
-    // Create a JSON file to import.
     $path = database_path('seeders' . DIRECTORY_SEPARATOR . 'events-test.json');
-
-    // Create events with localized start_date and end_date values.
     $seeds = Event::factory()->count(4)->make();
 
     file_put_contents($path, json_encode($seeds->toArray()));
 
-    // Seed the database.
     $seeder = new EventSeeder();
     $seeder->run();
-
-    // Check that the events were created with UTC datetimes in the database.
     $events = Event::all();
+
     expect($events->count())->toBe(4);
 });
