ea229edb85fd2495343551b79544a9f0f02a16ec
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-09 22:35:44 -0600

    Create new helper function for converting from a given timezone to UTC

--- app/helpers.php
+++ app/helpers.php
@@ -18,3 +18,18 @@ if (!function_exists('convertToUserTimezone')) {
         return $timestamp->setTimezone('UTC')->toDateTimeString();
     }
 }
+
+if (!function_exists('convertFromTimezone')) {
+    /**
+     * Convert a datetime string to UTC from the given timezone.
+     *
+     * @param string $datetime
+     * @param string $timezone
+     * @return string
+     */
+    function convertFromTimezone(string $datetime, string $timezone): string
+    {
+        $timestamp = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $datetime, $timezone);
+        return $timestamp->setTimezone('UTC')->toDateTimeString();
+    }
+}
