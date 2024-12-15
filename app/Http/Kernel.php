8ef8f3abbdf66821caab1b29ceb480a7825d4195
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-14 19:42:43 -0600

    Remove middleware alias

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'convert.event.timezone' => \App\Http\Middleware\ConvertEventTimezone::class,
     ];
 }
