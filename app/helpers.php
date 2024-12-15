41ea3b675f42b72d5abe636b1378fc65fea073d6
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-15 15:57:49 -0600

    Add helpers for converting session timezones to and from UTC datetimes

--- app/helpers.php
+++ app/helpers.php
@@ -4,6 +4,58 @@
  * This file defines global helper functions.
  */
 
+if (!function_exists('dateToSessionTime')) {
+    /**
+     * Convert a datetime string from UTC to the session's timezone.
+     *
+     * @param string $utcDatetime
+     * @return string
+     */
+    function dateToSessionTime(?string $utcDatetime): string | null
+    {
+        if (!$utcDatetime) {
+            return null;
+        }
+        $clientTimezoneOffset = session('timezoneOffset'); // minutes.
+        if (auth()->user?->timezone) {
+            $clientTimezoneOffset = (new \DateTimeZone(auth()->user->timezone))->getOffset(new \DateTime()) / 60;
+        }
+        if ($clientTimezoneOffset === 0) {
+            return $utcDatetime;
+        }
+        if ($clientTimezoneOffset > 0) {
+            return (new \DateTime($utcDatetime))->modify('+' . $clientTimezoneOffset . ' minutes')->format('Y-m-d\TH:i:s');
+        }
+        return (new \DateTime($utcDatetime))->modify($clientTimezoneOffset . ' minutes')->format('Y-m-d\TH:i:s');
+    }
+}
+
+if (!function_exists('dateFromSessionTime')) {
+    /**
+     * Convert a datetime string from the session's timezone to UTC.
+     *
+     * @param string $sessionDatetime
+     * @return string
+     */
+    function dateFromSessionTime(?string $sessionDatetime): string | null
+    {
+        if (!$sessionDatetime) {
+            return null;
+        }
+        $clientTimezoneOffset = session('timezoneOffset'); // minutes.
+        if (auth()->user?->timezone) {
+            $clientTimezoneOffset = (new \DateTimeZone(auth()->user->timezone))->getOffset(new \DateTime()) / 60;
+        }
+        if ($clientTimezoneOffset === 0) {
+            return $sessionDatetime;
+        }
+        if ($clientTimezoneOffset > 0) {
+            return (new \DateTime($sessionDatetime))->modify('-' . $clientTimezoneOffset . ' minutes')->format('Y-m-d\TH:i:s');
+        }
+        return (new \DateTime($sessionDatetime))->modify('+' . abs($clientTimezoneOffset) . ' minutes')->format('Y-m-d\TH:i:s');
+    }
+}
+
 if (!function_exists('convertFromUserTimezone')) {
     /**
      * Convert a datetime string from the user's timezone to UTC.
