748a3c6d6b3055cc2b6ebef1ef6a865c6f0bf468
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-15 21:58:29 -0600

    Make timezone field not required

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -25,7 +25,7 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
-            'timezone' => ['required', 'string', 'max:255'],
+            'timezone' => ['string', 'max:255'],
         ];
     }
 }
