38db241bbe23e3dc525bee83d94a38435a1bdb2a
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-19 19:22:45 -0600

    Update home page layout, footer (#40)

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -4,13 +4,9 @@ import { formatServerTimestamp } from "@/utils";
 
 export default function Welcome({
     auth,
-    laravelVersion,
-    phpVersion,
     events,
     now,
 }: PageProps<{
-    laravelVersion: string;
-    phpVersion: string;
     now: string;
     events: Event[];
 }>) {
@@ -28,84 +24,84 @@ export default function Welcome({
     return (
         <>
             <Head title="Welcome" />
-            <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-                <img
-                    id="background"
-                    className="absolute -left-20 top-0 max-w-[877px]"
-                    src="https://laravel.com/assets/img/welcome/background.svg"
-                />
-                <div className="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-                    <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                        <header className="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                            <div className="lg:col-start-2 lg:justify-center text-center">
-                                <h1 className="block text-2xl text-white">
-                                    Family Calendar
-                                </h1>
-                                {formatServerTimestamp(now)}
-                            </div>
-                            <nav className="-mx-3 flex flex-1 justify-end">
-                                {auth.user ? (
+            <div className="bg-white text-black/50 dark:bg-black dark:text-white/50 overflow-hidden">
+                <div className="relative">
+                    <img
+                        id="background"
+                        className="absolute -left-20 top-0 max-w-[877px]"
+                        src="https://laravel.com/assets/img/welcome/background.svg"
+                    />
+                </div>
+                <div className="relative flex min-h-screen flex-col items-center justify-center w-full max-w-2xl px-6 lg:max-w-7xl">
+                    <header className="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                        <div className="lg:col-start-2 lg:justify-center text-center">
+                            <h1 className="block text-2xl text-white">
+                                Family Calendar
+                            </h1>
+                            {formatServerTimestamp(now)}
+                        </div>
+                        <nav className="mx-3 flex flex-1 justify-end">
+                            {auth.user ? (
+                                <Link
+                                    href={route("dashboard")}
+                                    className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Dashboard
+                                </Link>
+                            ) : (
+                                <>
                                     <Link
-                                        href={route("dashboard")}
+                                        href={route("login")}
                                         className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
-                                        Dashboard
+                                        Log in
                                     </Link>
-                                ) : (
-                                    <>
-                                        <Link
-                                            href={route("login")}
-                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Log in
-                                        </Link>
-                                        <Link
-                                            href={route("register")}
-                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Register
-                                        </Link>
-                                    </>
-                                )}
-                            </nav>
-                        </header>
+                                    <Link
+                                        href={route("register")}
+                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Register
+                                    </Link>
+                                </>
+                            )}
+                        </nav>
+                    </header>
 
-                        <main className="mt-6">
-                            <div className="grid gap-6 lg:grid-cols-2 lg:gap-8">
-                                {/* List of events with days, minutes, and seconds until the event. */}
-                                {events &&
-                                    events.map((event) => (
-                                        <div
-                                            key={event.id}
-                                            className="flex flex-col items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
-                                        >
-                                            <div className="pt-3 sm:pt-5">
-                                                <h2 className="text-xl font-semibold text-black dark:text-white">
-                                                    {event.name} starts in{" "}
-                                                    {event.countdown}
-                                                </h2>
+                    <main className="mt-6 relative w-full max-w-2xl px-6 lg:max-w-7xl mt-auto mb-auto">
+                        <div className="grid gap-6 lg:grid-cols-2 lg:gap-8">
+                            {/* List of events with days, minutes, and seconds until the event. */}
+                            {events &&
+                                events.map((event) => (
+                                    <div
+                                        key={event.id}
+                                        className="flex flex-col items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
+                                    >
+                                        <div className="pt-3 sm:pt-5">
+                                            <h2 className="text-xl font-semibold text-black dark:text-white">
+                                                {event.name} starts in{" "}
+                                                {event.countdown}
+                                            </h2>
 
-                                                <p className="mt-4 text-sm/relaxed">
-                                                    {event.description}
-                                                </p>
+                                            <p className="mt-4 text-sm/relaxed">
+                                                {event.description}
+                                            </p>
 
-                                                <div className="flex items-center gap-2 mt-4">
-                                                    <span className="text-sm/relaxed">
-                                                        {formatServerTimestamp(
-                                                            event.start_date
-                                                        )}
-                                                    </span>
-                                                </div>
+                                            <div className="flex items-center gap-2 mt-4">
+                                                <span className="text-sm/relaxed">
+                                                    {formatServerTimestamp(
+                                                        event.start_date
+                                                    )}
+                                                </span>
                                             </div>
                                         </div>
-                                    ))}
-                            </div>
-                        </main>
+                                    </div>
+                                ))}
+                        </div>
+                    </main>
 
-                        <footer className="py-16 text-center text-sm text-black dark:text-white/70">
-                            Laravel v{laravelVersion} (PHP v{phpVersion})
-                        </footer>
-                    </div>
+                    <footer className="py-16 text-center text-sm text-black dark:text-white/70">
+                        &copy; {new Date().getFullYear()} Zachary Watkins
+                    </footer>
                 </div>
             </div>
         </>
