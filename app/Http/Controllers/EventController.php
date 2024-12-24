0c298edeb51a1f0f68f9c38a92da60c3ec7db95f
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-24 12:12:43 -0600

    Remove unused imports

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,12 +5,9 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use App\Models\Event;
 use Inertia\Response;
-use Illuminate\View\View;
 use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use App\Http\Requests\EventStoreRequest;
-use Illuminate\Support\Facades\Redirect;
 use App\Http\Requests\EventUpdateRequest;
 
 class EventController extends Controller
