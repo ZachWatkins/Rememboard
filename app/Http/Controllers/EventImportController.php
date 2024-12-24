1182c92c74fda81edc10784d91ddfae09edff21a
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-24 15:06:11 -0600

    Implement file adapter

--- app/Http/Controllers/EventImportController.php
+++ app/Http/Controllers/EventImportController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Event;
 use Inertia\Response;
+use Illuminate\Http\Request;
+use App\Services\File\IcsFileAdapter;
 
 class EventImportController extends Controller
 {
@@ -14,7 +15,7 @@ class EventImportController extends Controller
         return Inertia::render('Event/Import');
     }
 
-    public function upload(Request $request)
+    public function upload(Request $request, IcsFileAdapter $adapter)
     {
         $request->validate([
             'file' => 'required|file|mimes:ics',
@@ -22,7 +23,8 @@ class EventImportController extends Controller
 
         $path = $request->file('file')->store('temp');
 
-        $events = Event::import($path);
+        $events = $adapter->getEvents($path);
+        $events->each(fn(Event $event) => $event->save());
 
         return redirect()->back()->with('events', $events);
     }
