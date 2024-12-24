e72d022a2d3c2fbde1abd0bb4042e19364fd3c54
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-24 12:14:32 -0600

    Create controller for Event Import page

---
+++ app/Http/Controllers/EventImportController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Event;
+use Inertia\Response;
+
+class EventImportController extends Controller
+{
+    public function import(): Response
+    {
+        return Inertia::render('Event/Import');
+    }
+
+    public function upload(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|mimes:ical,txt',
+        ]);
+
+        $path = $request->file('file')->store('temp');
+
+        $events = Event::import($path);
+
+        return redirect()->back()->with('events', $events);
+    }
+}
