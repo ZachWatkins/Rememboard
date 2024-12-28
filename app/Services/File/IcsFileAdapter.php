4498a76d0ba650140c7138d7c3ef5d0561a7bc52
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-27 18:35:58 -0600

    Reimplement method as a generator for lower memory usage

--- app/Services/File/IcsFileAdapter.php
+++ app/Services/File/IcsFileAdapter.php
@@ -3,19 +3,18 @@
 namespace App\Services\File;
 
 use App\Models\Event;
-use Illuminate\Support\Collection;
 use Sabre\VObject;
 
 class IcsFileAdapter
 {
     /**
-     * Get events from an .ics file.
+     * Get events from a .ics file.
      * @param string $path Path to a file in app/private.
      */
-    public function getEvents(string $path): Collection
+    public function getEvents(string $path): \Generator
     {
         if ('ics' !== pathinfo($path, PATHINFO_EXTENSION)) {
-            throw new \InvalidArgumentException('File must be an .ics file');
+            throw new \InvalidArgumentException('File must be a .ics file');
         }
 
         $path = storage_path('app/private/' . $path);
@@ -24,18 +23,20 @@ class IcsFileAdapter
         }
 
         $vcalendar = VObject\Reader::read(fopen($path, 'r'), VObject\Reader::OPTION_FORGIVING);
+        $names = [];
 
-        $events = collect($vcalendar->VEVENT)
-            ->map(function (VObject\Component\VEvent $vevent) {
+        foreach ($vcalendar->VEVENT as $vevent) {
+            $name = $vevent->SUMMARY->getValue();
+            if (!\in_array($name, $names, true)) {
+                $names[] = $name;
                 $event = new Event();
-                $event->name = $vevent->SUMMARY->getValue();
+                $event->name = $name;
                 $event->description = $vevent->DESCRIPTION?->getValue() ?? '';
                 $event->start_date = $vevent->DTSTART->getDateTime()->format('Y-m-d H:i:s');
                 $event->end_date = $vevent->DTEND->getDateTime()->format('Y-m-d H:i:s');
                 $event->address = $vevent->LOCATION?->getValue() ?? '';
-                return $event;
-            });
-
-        return $events;
+                yield $event;
+            }
+        }
     }
 }
