e956480f747fcbe7c893a41148cc2781e679cb65
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-12-29 13:33:25 -0600

    Remove unused imports

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Models\Event;
-use App\Models\Participant;
-use App\Services\GeolocationService;
 use Illuminate\Foundation\Inspiring;
-use App\Services\File\IcsFileAdapter;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
