cce37cfd06e731a4f4e8610cff9e9b10e2352596
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-01-09 20:57:30 -0600

    Expand service

--- app/Services/AddressParsingService.php
+++ app/Services/AddressParsingService.php
@@ -4,6 +4,39 @@ namespace App\Services;
 
 class AddressParsingService
 {
+    public function getCity(string $address): string | null
+    {
+        if ($address) {
+            $state = $this->getState($address);
+            if ($state) {
+                if (preg_match("/([a-zA-Z\s\-']+)[,\s]*{$state}/", $address, $matches)) {
+                    return trim($matches[1], ' ,');
+                }
+            }
+        }
+        return null;
+    }
+
+    public function getState(string $address): string | null
+    {
+        if ($address) {
+            if (preg_match('/\bTX\b|\bTexas\b/', $address, $matches)) {
+                return $matches[0];
+            }
+        }
+        return null;
+    }
+
+    public function getZip(string $address): string | null
+    {
+        if ($address) {
+            if (preg_match('/\b(\d{5})(-\d{4})?\b/', $address, $matches)) {
+                return $matches[1];
+            }
+        }
+        return null;
+    }
+
     public function getCountry(string $address): string | null
     {
         if ($address) {
